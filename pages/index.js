4339a3ed35110b2fd06e277174087a70c0c82718
aljorgevi <jormencar@yahoo.com>
2022-09-20 23:14:58 +0100

    add hero

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,27 @@
 import Layout from '../components/Layout';
+import { createClient } from 'contentful';
+import Hero from '../components/Hero';
 
-// import { createClient } from 'contentful';
-
-// const client = createClient({
-// 	space: process.env.CONTENTFUL_SPACE_ID,
-// 	accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-// });
+const client = createClient({
+	space: process.env.CONTENTFUL_SPACE_ID,
+	accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+});
 
 export default function Home({ images }) {
-	console.log({ images });
 	return (
 		<>
-			<Layout>hello world</Layout>
+			<Layout>
+				<Hero images={images} />
+			</Layout>
 		</>
 	);
 }
 
 export async function getStaticProps() {
-	// const res = await client.getEntries({ content_type: 'portfolio' });
+	const res = await client.getEntries({ content_type: 'containers' });
 	return {
 		props: {
-			images: [1, 2, 3]
+			images: res.items
 		}
 	};
 }
