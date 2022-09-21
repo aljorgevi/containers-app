d29537fb16cb3d825bdbad1f258d5698bdc4a477
aljorgevi <jormencar@yahoo.com>
2022-09-21 17:05:57 +0100

    add slider

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,17 +1,46 @@
-export default function Hero({ images }) {
-	return (
-		<section>
-			{images.map(img => {
-				const { sys, fields } = img;
-				const { image, title } = fields;
-				const { fields: imgFields } = image;
+import { useEffect, useState } from 'react';
+import styles from '../styles/Hero.module.css';
 
-				return (
-					<div key={sys.id}>
-						<img src={imgFields.file.url} alt={title} />
+export default function Hero({ images }) {
+	const [slide, setSlide] = useState(0);
+
+	const goToNext = index => {
+		setSlide(index);
+	};
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (slide === images.length - 1) return setSlide(0);
+
+			setSlide(slide + 1);
+		}, 5000);
+
+		return () => clearTimeout(timer);
+	}, [slide, images.length]);
+
+	return (
+		<section className={styles.Hero}>
+			<div className={styles.HeroContainer}>
+				<div
+					style={{
+						backgroundImage: `url(${images[slide].url})`,
+						backgroundPosition: 'center',
+						backgroundSize: 'cover',
+						height: '100%'
+					}}
+				></div>
+				<div className={styles.HeroDescripion}>
+					<div>
+						<h1>{images[slide].title}</h1>
+						<p>{images[slide].description}</p>
 					</div>
-				);
-			})}
+					<div className={styles.HeroCarousel}>
+						{images.map((img, index) => {
+							return <span key={img.id} onClick={() => goToNext(index)}></span>;
+						})}
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 }
