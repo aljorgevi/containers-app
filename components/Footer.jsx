868f167c16e6aaf60ebc02aeb6502513623b1235
aljorgevi <jormencar@yahoo.com>
2022-09-22 22:56:04 +0100

    add footer

---
+++ components/Footer.jsx
@@ -0,0 +1,33 @@
+import styles from '../styles/Footer.module.css';
+
+export default function Footer() {
+	return (
+		<section className='section-center'>
+			<div className={styles.Footer}>
+				<article className='contact'>
+					<h3>Contact</h3>
+					<div>
+						<p>email</p>
+						<p>week days</p>
+						<p>horario</p>
+						<button>Contactanos!</button>
+					</div>
+				</article>
+				<article className={styles.Social}>
+					<h3>Social</h3>
+					<div>iconos</div>
+				</article>
+				<article className={styles.Subscription}>
+					<h3>Subscribe</h3>
+					<p>descripcion</p>
+					<input type='text' name='' id='' />
+					<button>Subscribirme!</button>
+				</article>
+				<article className={styles.Location}>
+					<h3>Ubicacion</h3>
+					<p>direccion</p>
+				</article>
+			</div>
+		</section>
+	);
+}
