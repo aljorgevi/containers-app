76a32981fe5a1f0666c03f6b6df71a85715efb46
aljorgevi <jormencar@yahoo.com>
2022-11-13 01:29:03 +0000

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,64 @@
-import styles from '../../styles/Footer.module.css';
+import styled from 'styled-components';
+import Button from '@mui/material/Button';
 
 export default function Footer() {
 	return (
-		<section className='section-center'>
-			<div className={styles.Footer}>
+		<Wrapper className='section-center'>
+			<div className='center'>
 				<article className='contact'>
-					<h3>Contact</h3>
+					<h4>Contact</h4>
 					<div>
 						<p>email</p>
 						<p>week days</p>
 						<p>horario</p>
-						<button>Contactanos!</button>
+						{/* TODO: change the colour primary to loki colours  */}
+						<Button variant='outlined'>Contactanos!</Button>
 					</div>
 				</article>
-				<article className={styles.Social}>
+				<article className='social'>
 					<h3>Social</h3>
 					<div>iconos</div>
 				</article>
-				<article className={styles.Subscription}>
+				<article className='subscription'>
 					<h3>Subscribe</h3>
 					<p>descripcion</p>
 					<input type='text' name='' id='' />
 					<button>Subscribirme!</button>
 				</article>
-				<article className={styles.Location}>
+				<article className='location'>
 					<h3>Ubicacion</h3>
 					<p>direccion</p>
 				</article>
 			</div>
-		</section>
+		</Wrapper>
 	);
 }
+
+const Wrapper = styled.section`
+	.center {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		padding: 50px 0;
+		flex-wrap: wrap;
+	}
+
+	.contact {
+	}
+
+	article {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-align: center;
+	}
+
+	.social {
+	}
+
+	.subscription {
+	}
+
+	.location {
+	}
+`;
