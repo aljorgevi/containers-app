8de630b35c526754adf30b81520de1772cc348ed
aljorgevi <jormencar@yahoo.com>
2022-11-13 14:57:50 +0000

    add socialLinks

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,29 +1,26 @@
-import { FaInstagram, FaFacebookF, FaWhatsapp } from 'react-icons/fa';
-import Link from 'next/link';
+import PropTypes from 'prop-types';
+import { socialLinks } from '../constants';
 
-const SocialLinks = ({ styleClass }) => {
+const SocialLinks = ({ className }) => {
 	return (
-		<ul className={styleClass}>
-			<li>
-				<a href='www.linkedin.com/in/jorgeamena'>
-					<FaInstagram className='social-icon linkedin-icon' />
-				</a>
-			</li>
-			<li>
-				<Link href='https://github.com/aljorgevi'>
-					<a>
-						<FaFacebookF className='social-icon github-icon' />
-					</a>
-				</Link>
-			</li>
-			<li>
-				<Link href='https://github.com/aljorgevi'>
-					<a>
-						<FaWhatsapp className='social-icon github-icon' />
-					</a>
-				</Link>
-			</li>
+		<ul className={className}>
+			{socialLinks.map(item => {
+				const { url, id, icon, rel, target } = item;
+
+				return (
+					<li key={id}>
+						<a href={url} rel={rel} target={target}>
+							{icon}
+						</a>
+					</li>
+				);
+			})}
 		</ul>
 	);
 };
+
 export default SocialLinks;
+
+SocialLinks.propTypes = {
+	className: PropTypes.string
+};
