166f398e830117acf99a11f278addea0511031d5
aljorgevi <jormencar@yahoo.com>
2022-11-13 14:58:27 +0000

    create constans

---
+++ src/constants/index.js
@@ -0,0 +1,25 @@
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+export const socialLinks = [
+	{
+		id: 1,
+		icon: <FaTwitter />,
+		target: '_blank',
+		rel: 'noreferrer',
+		url: 'https://twitter.com/lokirentacar'
+	},
+	{
+		id: 2,
+		icon: <FaFacebook />,
+		target: '_blank',
+		rel: 'noreferrer',
+		url: 'https://www.facebook.com/lokicars.cl/'
+	},
+	{
+		id: 3,
+		icon: <FaInstagram />,
+		target: '_blank',
+		rel: 'noreferrer',
+		url: 'https://www.instagram.com/arriendo_de_autos_puerto_montt'
+	}
+];
