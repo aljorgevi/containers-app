ebc63c0a8afb33739b2d6ba406e5130a7001b8c7
aljorgevi <jormencar@yahoo.com>
2022-11-14 18:55:39 +0000

    add material theme

---
+++ src/config/materialTheme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+	palette: {
+		primary: { main: '#ffda1f' },
+		secondary: {
+			main: '#fff'
+		},
+		info: {
+			main: '#3a3a3a'
+		},
+		multilineColor: {
+			color: 'red'
+		}
+	}
+});
