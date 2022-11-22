9edf7dbf6fe6a180a71096dc6dcd01217445ce04
aljorgevi <jormencar@yahoo.com>
2022-11-22 18:21:35 +0000

    add proptypes

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,14 +1,15 @@
-import Button from '@mui/material/Button';
-import styles from './Button.module.css';
+import Button from "@mui/material/Button";
+import styles from "./Button.module.css";
+import PropTypes from "prop-types";
 
 export default function CustomButton({ children, className }) {
 	return (
-		<Button
-			color='primary'
-			className={`${styles.Button} ${className}`}
-			variant='outlined'
-		>
+		<Button color="primary" className={`${styles.Button} ${className}`} variant="outlined">
 			{children}
 		</Button>
 	);
 }
+
+CustomButton.propTypes = {
+	className: PropTypes.string,
+};
